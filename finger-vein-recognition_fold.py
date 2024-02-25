c5621c15dbf02e5e14a38464ace1b625d413648d
yahfou <yang.mingham@gmail.com>
2024-02-26 02:48:40 +0800

    Update finger-vein-recognition_fold.py

--- finger-vein-recognition_fold.py
+++ finger-vein-recognition_fold.py
@@ -8,25 +8,25 @@ for filename in os.listdir(directory_name):
     for finger in os.listdir(directory_name+'/'+filename):
         img = cv2.imread(directory_name+'/'+ filename +'/'+ finger)
         title = re.findall('[0-9]{1,}' , finger)
+        man_type = title[2]
         finger_type = title[3]
         
         if finger_type == '02':            
-            cv2.imwrite('C:/Users/yahfou/Desktop/dataset/02/'+finger,img)
+            cv2.imwrite('C:/Users/yahfou/Desktop/datasett/02/'+man_type+'/'+finger,img)
             
         if finger_type == '03':
-            cv2.imwrite('C:/Users/yahfou/Desktop/dataset/03/'+finger,img)
+            cv2.imwrite('C:/Users/yahfou/Desktop/datasett/03/'+man_type+'/'+finger,img)
             
         if finger_type == '04':
-            cv2.imwrite('C:/Users/yahfou/Desktop/dataset/04/'+finger,img)
+            cv2.imwrite('C:/Users/yahfou/Desktop/datasett/04/'+man_type+'/'+finger,img)
             
         if finger_type == '07':
-            cv2.imwrite('C:/Users/yahfou/Desktop/dataset/07/'+finger,img)
+            cv2.imwrite('C:/Users/yahfou/Desktop/datasett/07/'+man_type+'/'+finger,img)
             
         if finger_type == '08':
-            cv2.imwrite('C:/Users/yahfou/Desktop/dataset/08/'+finger,img)
+            cv2.imwrite('C:/Users/yahfou/Desktop/datasett/08/'+man_type+'/'+finger,img)
         
         if finger_type == '09':
-            cv2.imwrite('C:/Users/yahfou/Desktop/dataset/09/'+finger,img)
-                
+            cv2.imwrite('C:/Users/yahfou/Desktop/datasett/09/'+man_type+'/'+finger,img)
             
             
